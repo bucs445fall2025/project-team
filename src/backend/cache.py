d86c65b25e5fa580a54ba011950ea9e45981bfaa
ChinnyChilla <michaelzheng04@gmail.com>
2025-10-08 12:34:37 -0400

    updated classes

--- src/backend/cache.py
+++ src/backend/cache.py
@@ -1,10 +1,6 @@
-class APICache:
+class CacheType:
 	def __init__(self):
-		self.num_objects = 0
-		self.stored_objects = []
-
-class CacheDataMember:
-	def __init__(self):
-		self.expiration = 0
-		self.ticker = ""
 		self.data = {}
+
+class RealPrice(CacheType):
+	pass
