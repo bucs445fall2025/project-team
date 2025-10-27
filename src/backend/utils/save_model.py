4d5610490b0c59ab5066b846919d83804d71c343
brendon-pa <bpaolin1@binghamton.edu>
2025-10-27 12:51:31 -0400

    fix save_model bug

--- src/backend/utils/save_model.py
+++ src/backend/utils/save_model.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 def save_model(model_state, path: str):
-	if self.X_mean is None or self.X_std is None:
+	if model_state.X_mean is None or model_state.X_std is None:
 		print("Model must be populated before making predictions")
 		return False
 	os.makedirs(os.path.dirname(path), exist_ok=True)
