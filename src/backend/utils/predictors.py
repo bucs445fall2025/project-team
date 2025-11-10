10522901b07918195434b4b67ffea428a05a1762
brendon-pa <bpaolin1@binghamton.edu>
2025-11-09 21:57:42 -0500

    create timer decorator/wrapper for printing the time it takes for a function to run

--- src/backend/utils/predictors.py
+++ src/backend/utils/predictors.py
@@ -5,6 +5,7 @@ from utils.load_model import load_model
 from model.linear import create_model
 from utils.get_sp500 import get_sp500
 import concurrent.futures
+from utils.timer import timer
 
 DIR = "cached_models/"
 MODEL_TYPE = "LINEAR"
@@ -21,7 +22,7 @@ def run_all_predictions(file_path: str=DIR, input: str=TARGET_DATE, model_type: 
 			if file.endswith(".pth"):
 				run_prediction(os.path.join(root, file), os.path.basename(root), input, device) """
 
-
+@timer
 def run_all_predictions(file_path: str=DIR, input: str=TARGET_DATE, model_type: str=MODEL_TYPE, device: str="cpu"):
 	file_path = os.path.join(file_path, model_type)
 	if not os.path.exists(file_path):
@@ -45,15 +46,17 @@ def run_all_predictions(file_path: str=DIR, input: str=TARGET_DATE, model_type: 
 			except Exception as e:
 				print(f'Ticker {ticker} generated an exception: {e}')
 
-def run_prediction(file_path: str, ticker: str, input: str=TARGET_DATE, device: str="cpu"):
-	model = load_model(file_path, ticker, device)
-	print(f"Loaded model for {ticker} from {file_path}")
-	prediction = model.predict(input)
-	print(f"[{file_path[len(DIR):]}] Prediction for {input}: ${prediction:.2f}")
+def run_prediction(file_path: str, ticker: str, input: str=TARGET_DATE, device: str="cpu", insert_to_db=True):
 	try:
+		model = load_model(file_path, ticker, device)
+		print(f"Loaded model for {ticker} from {file_path}")
+		prediction = model.predict(input)
+		print(f"[{file_path[len(DIR):]}] Prediction for {input}: ${prediction:.2f}")
 		db.insert_prediction(ticker, round(prediction, 2))
+		return True
 	except Exception as e:
 		print(f"Error while posting to database: {e}")
+		return False
 
 def save_sp500(count: int = 500, file_path: str=DIR, model_type: str=MODEL_TYPE):
 	sp500_tickers = get_sp500()
