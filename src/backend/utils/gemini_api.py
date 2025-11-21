54ef3cd61925931f3043043579c2a7dd7b14bc26
G.S <gavinsuber13@gmail.com>
2025-11-20 20:11:25 -0500

    added caching for gemini

--- src/backend/utils/gemini_api.py
+++ src/backend/utils/gemini_api.py
@@ -1,12 +1,20 @@
 import os
-import requests
+import mysql.connector
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
-from utils.yfinance_api import get_stock_data
+from yfinance_api import get_stock_data
 from google import genai
 
 load_dotenv()
 
+def get_db_connection():
+    return mysql.connector.connect(
+        user=os.getenv("DATABASE_USERNAME"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        host="db.chinny.net",
+        database=os.getenv("DATABASE_BASE"),
+        port=3306,
+    )
 
 class gemini_api:
 	def __init__(self, model: str = "gemini-2.5-flash"):
@@ -36,8 +44,44 @@ class gemini_api:
 					raise RuntimeError(f"Gemini API request failed after 5 attempts: {str(e)}")
 		return response.text
 
+	# save analysis to cache
+	def save_to_cache(self, ticker: str, analysis: str):
+		conn = get_db_connection()
+		cursor = conn.cursor()
+		expires = (datetime.now() + timedelta(hours=24))
+		cursor.execute(
+			"INSERT INTO gemini_cache (ticker, analysis, created, expires) VALUES (%s, %s, %s, %s)",
+			(ticker, analysis, datetime.now(), expires)
+		)
+		conn.commit()
+		cursor.close()
+		conn.close()
+
+	# retrieve cached analysis if exists and not expired
+	def get_cached_gemini(self, ticker:str):
+		conn = get_db_connection()
+		cursor = conn.cursor(dictionary=True)
+		cursor.execute(
+			"SELECT * FROM gemini_cache WHERE ticker = %s AND expires > %s ORDER BY created DESC LIMIT 1",
+			(ticker, datetime.now())
+		)
+		row = cursor.fetchone()
+		cursor.close()
+		conn.close()
+
+		if row:
+			return row['analysis']
+		else:
+			return None
+	
 	def analyze_stock(self, ticker: str) -> str:
 		
+		# check cache
+		cached = self.get_cached_gemini(ticker)
+		if cached:
+			return cached
+		# continue and cache later
+
 		# get stock data for past 5 days and take just the last 3 days
 		today = datetime.today().strftime("%Y-%m-%d")
 		five_days_ago = (datetime.today() - timedelta(days=5)).strftime("%Y-%m-%d")
@@ -75,11 +119,14 @@ class gemini_api:
 			f"Any potential risks or opportunities.\n"
 			f"Provide a concise but insightful analysis."
 		)
+	
+		# save to cache
+		gemini = self._query_gemini(prompt)
+		self.save_to_cache(ticker, gemini)
+		return gemini
 
-		return self._query_gemini(prompt)
 
-
-if __name__ == "__main__":
-	gemini = gemini_api()
-	analysis = gemini.analyze_stock("GLW")
-	print(analysis)
+#if __name__ == "__main__":
+#	gemini = gemini_api()
+#	analysis = gemini.analyze_stock("GLW")
+#	print(analysis)
